548ec2996028c6b15be6cf60987bcf0c8672d075
koemsakdev <koemsak.mean@gmail.com>
2025-05-29 17:38:56 +0700

    Update search and filter by legend cinema

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,101 @@
 export default {
     name: "Navbar",
+    data() {
+        return {
+            isOpen: false,
+            cinemas: [],
+            cinema: undefined,
+            activeCinemaId: null,
+            cinemaApi: "https://api.legend.com.kh/cinemas",
+            isLoading: false,
+            search: "",
+            searchResults: [],
+            isSearch: false,
+            isInputSearch: false,
+        };
+    },
+    methods: {
+        async fetchCinemas() {
+            try {
+                const response = await fetch(this.cinemaApi);
+                const data = await response.json();
+                this.cinemas = data;
+            } catch (error) {
+                console.error("Error fetching cinemas:", error);
+            }
+        },
+        async handleChange(id) {
+            this.activeCinemaId = id;
+            this.cinema = this.cinemas.find(cinema => cinema.vistaCinemaId === id);
+            this.isOpen = false;
+        },
+        toggleDropdown() {
+            this.isOpen = !this.isOpen;
+        },
+        handleClickOutside(event) {
+            const dropdown = this.$refs.cinemaDropdown;
+            if (dropdown && !dropdown.contains(event.target)) {
+                this.isOpen = false;
+            }
+        },
+        async handleSearch() {
+            let limit = 10;
+            let api = `https://api.legend.com.kh/films?limit=${limit}`;
+            if (this.search.trim() === '') {
+                this.isSearch = false;
+                api = `https://api.legend.com.kh/scheduled-films?limit=${limit}`;
+                this.searchResults = [];
+            } else {
+                this.isSearch = true;
+                limit = 100;
+                api = `${api}&searchText=${this.search}`;
+            }
+            try {
+                const response = await fetch(api);
+                const data = await response.json();
+                this.searchResults = data.rows;
+                this.isLoading = false;
+            } catch (error) {
+                this.isLoading = false;
+                console.error("Error fetching cinemas:", error);
+            }
+
+            console.log(api);
+        },
+        handleClearSearch() {
+            this.search = '';
+            this.isSearch = false;
+            this.isInputSearch = false;
+        },
+        async handleFocus() {
+            this.isInputSearch = true;
+            this.handleSearch();
+        },
+        handleBlur() {
+            this.isInputSearch = false;
+        },
+        formatDate(date) {
+            const dateObj = new Date(date);
+            const day = dateObj.getDate();
+            const month = dateObj.toLocaleString('default', { month: 'short' });
+            const year = dateObj.getFullYear();
+            return `${day} ${month} ${year}`;
+        }
+    },
+    mounted() {
+        this.fetchCinemas();
+        document.addEventListener('click', this.handleClickOutside);
+    },
+    beforeDestroy() {
+        document.removeEventListener('click', this.handleClickOutside);
+    },
     template: `
         <nav class="sticky left-0 top-0 z-[9999] w-full bg-secondary/50 backdrop-blur-2xl px-4 md:px-6">
-            <div class="container max-w-6xl py-5 flex flex-col m-auto">
-                <div class="flex flex-row-reverse md:flex-row lg:flex-row items-center justify-between relative">
+            <div class="container max-w-6xl flex flex-col m-auto">
+                <div class="flex flex-row-reverse md:flex-row lg:flex-row items-center justify-between relative py-4 lg:py-6">
                     <!-- Search -->
                     <div class="relative max-full order-1 md:order-1 z-10">
-                        <input type="text" name="search" id="search" class="w-full font-light grow py-2 px-4 text-base text-white placeholder:text-gray-400 focus:outline-none sm:text-sm/6 rounded-full bg-gray-900/50 pr-10 ring-1 ring-gray-400/25 focus:ring-gray-400/50 hidden md:block" placeholder="Search Movies" />
+                        <input type="text" v-model="search" @blur="handleBlur" @focus="handleFocus" @input="handleSearch" id="search" class="w-full font-light grow py-2 px-4 text-base text-white placeholder:text-gray-400 focus:outline-none sm:text-sm/6 rounded-full bg-gray-900/50 pr-10 ring-1 ring-gray-400/25 focus:ring-gray-400/50 hidden md:block" placeholder="Search Movies" />
                         <button class="md:hidden text-gray-200 hover:text-gray-100 transition-colors cursor-pointer">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor" class="size-6 mt-2.5">
@@ -14,12 +103,43 @@ export default {
                                 d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                             </svg>
                         </button>
-                        <div class="absolute inset-y-0 right-0 flex items-center text-gray-300 hover:text-gray-200 pr-4 hidden md:flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        <div class="absolute right-0 top-1/2 flex -translate-x-4 -translate-y-1/2 cursor-pointer items-center gap-2 text-gray-200">
+                            <svg v-if="!isSearch" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                 d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                             </svg>
+                            <svg v-else @click="handleClearSearch()" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="gridicons:cross">
+                                    <path 
+                                        id="Vector" 
+                                        d="M13.4577 14.6666L8.00433 9.20814L2.55095 14.6666L1.33337 13.4498L6.79532 7.99992L1.33337 2.55005L2.55095 1.33325L8.00433 6.79169L13.4577 1.34182L14.6667 2.55005L9.21333 7.99992L14.6667 13.4498L13.4577 14.6666Z" 
+                                        fill="white"
+                                    >
+                                    </path>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="absolute left-0 top-[50px] z-[50] h-[488px] max-h-[488px] w-[300px] overflow-auto rounded-xl border border-gray-200 bg-black p-6 md:w-[446px] lg:top-[67px]" :class="[isInputSearch ? 'transition block' : 'transition hidden']">
+                            <h4 class="text-2xl font-semibold text-gray-200 mb-4">Search</h4>
+                            <ul>
+                                <li
+                                    v-for="(result, index) in searchResults"
+                                    :key="index"
+                                    class="[&:not(:first-child)]:pt-4 [&:not(:last-child)]:border-b [&:not(:last-child)]:pb-4 [&:not(:last-child)]:gradient-border"
+                                >
+                                    <a href="#" class="flex gap-4">
+                                        <div class="h-[110px] w-[76px] overflow-hidden rounded">
+                                            <img :src="result.bannerImageUrl" :alt="result.title" class="img w-full h-full object-cover" />
+                                        </div>
+                                        <div class="flex flex-col gap-2">
+                                            <span class="line-clamp-3 text-base font-semibold text-gray-200">{{result.title}}</span>
+                                            <span class="text-xs text-gray-400">{{ formatDate(result.openingDate) }}</span>
+                                        </div>
+                                    </a>
+                                    <div v-if="index !== searchResults.length - 1" class="divider my-4"></div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                     <!-- Logo -->
@@ -59,11 +179,11 @@ export default {
                 </div>
             </div>
             <!-- Seperator as gradiant -->
-            <div class="divider mb-2 mt-1"></div>
+            <div class="divider"></div>
             <div class="container max-w-6xl flex flex-col m-auto">
-                <div class="flex items-center justify-between mb-4">
+                <div class="flex items-center justify-between">
                     <!-- Navigation -->
-                    <nav class="flex flex-col sm:flex-row sm:justify-between sm:items-center sm:gap-x-4 mt-4 md:mt-0 hidden md:flex">
+                    <nav class="flex flex-col py-4 sm:flex-row sm:justify-between sm:items-center sm:gap-x-4 hidden md:flex">
                         <ul class="flex flex-col sm:flex-row sm:items-center gap-x-3 sm:gap-x-6 text-sm font-medium text-gray-200">
                             <li>
                                 <a href="#" class="hover:text-gray-100 text-base text-gray-200 font-base hover:font-bold transition-colors cursor-pointer flex items-center gap-x-3">
@@ -93,56 +213,58 @@ export default {
                     </nav>
 
                     <!-- Select location -->
-                    <div class="relative w-full sm:w-full md:w-auto lg:w-auto">
-                        <select id="location" class="w-full font-light grow py-2 px-4 text-base text-white placeholder:text-gray-400 focus:outline-none sm:text-sm/6 rounded-full pl-12 cursor-pointer bg-transparent min-w-full" style="background: transparent">
-                            <option value="1" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                All Cinemas
-                            </option>
-                            <option value="2" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend Cinema 271 Mega Mall
-                            </option>
-                            <option value="3" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend Cinema 271 Mega Mall
-                            </option>
-                            <option value="4" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend Eden Garden
-                            </option>
-                            <option value="5" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend K Mall
-                            </option>
-                            <option value="6" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend Meanchey
-                            </option>
-                            <option value="7" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend Midtown Mall
-                            </option>
-                            <option value="8" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend Noro Mall
-                            </option>
-                            <option value="9" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend Olympia
-                            </option>
-                            <option value="10" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend Premium Exchange Square
-                            </option>
-                            <option value="11" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend SenSok
-                            </option>
-                            <option value="12" class="bg-slate-900 text-white hover:bg-slate-800 p-3">
-                                Legend Siem Reap
-                            </option>
-                            <option value="13" class="bg-gray-900 text-white hover:bg-gray-800 p-3">
-                                Legend Toul Kork
-                            </option>
-                        </select>
-                        <div class="absolute inset-y-0 left-0 flex items-center text-gray-400 hover:text-gray-300 pl-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="size-5 text-red-500">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                    <div class="relative w-full sm:w-full md:w-auto lg:w-auto" ref="cinemaDropdown">
+                        <button class="flex items-center gap-2 w-full justify-between py-2.5 lg:py-4 cursor-pointer" @click="toggleDropdown">
+                            <div class="flex text-left gap-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" class="size-5 text-red-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                </svg>
+                                <span class="text-sm text-gray-200 font-light line-clamp-1">{{cinema == undefined ? 'All Cinemas' : cinema.name}}</span>
+                            </div>
+                            <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg" :class="[isOpen ? 'transition rotate-180' : 'transition']">
+                                <path 
+                                    d="M0.594154 0.592566L0.594622 0.592089C0.66344 0.521721 0.72368 0.5 0.798736 0.5C0.873738 0.5 0.933524 0.521663 1.00175 0.591612L1.00222 0.592089L5.63463 5.3288L5.99209 5.69432L6.34956 5.3288L10.9978 0.575923C11.0463 0.526343 11.0995 0.5 11.1937 0.5C11.2772 0.5 11.3391 0.524277 11.4054 0.59209C11.4744 0.662668 11.5 0.729488 11.5 0.816719C11.5 0.903889 11.4745 0.97023 11.4059 1.04017C11.4059 1.04019 11.4059 1.04021 11.4058 1.04023L6.07783 6.47204L6.07732 6.47257C6.06745 6.48265 6.06072 6.48766 6.05762 6.48974C6.0547 6.49169 6.05442 6.49145 6.05653 6.49068C6.04281 6.49573 6.02354 6.50014 5.99465 6.5L5.99465 6.49999L5.99209 6.49999C5.96242 6.49999 5.94224 6.4954 5.92766 6.49003C5.92936 6.49066 5.92878 6.49072 5.92567 6.48865C5.92243 6.48649 5.91576 6.48154 5.90612 6.4718C5.90598 6.47166 5.90584 6.47151 5.9057 6.47137L0.578812 1.02454C0.527362 0.971928 0.5 0.913489 0.5 0.816072C0.5 0.72888 0.52555 0.662528 0.594154 0.592566Z"
+                                    fill="white" 
+                                    stroke="white"
+                                >
+                                </path>
                             </svg>
-                        </div>
+                        </button>
+
+                        <ul class="absolute max-h-[60vh] overflow-y-auto rounded-xl border bg-black p-3 lg:p-4 right-0 border-gray-200/50 w-full lg:w-[400px] xl:w-[544px] transition" :class="[isOpen ? 'block' : 'hidden']">
+                            <li class="cursor-pointer" @click="handleChange(null)">
+                                <div class="flex items-center gap-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="currentColor" class="size-5 text-red-500">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                    </svg>
+                                    <span :class="[activeCinemaId === null ? 'text-red-500' : 'text-gray-200', 'text-base font-bold line-clamp-1']">All Cinemas</span>
+                                </div>
+                                <div class="divider my-4"></div>
+                            </li>
+                            <li
+                                v-for="(cinema, index) in cinemas"
+                                :key="index"
+                                @click="handleChange(cinema.vistaCinemaId)"
+                                class="cursor-pointer"
+                            >
+                                <div class="flex items-center gap-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                        stroke="currentColor" class="size-5 text-red-500">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                    </svg>
+                                    <span :class="[activeCinemaId === cinema.vistaCinemaId ? 'text-red-500' : 'text-gray-200', 'text-base font-bold line-clamp-1']">{{cinema.name}}</span>
+                                </div>
+                                <div v-if="index !== cinemas.length - 1" class="divider my-4"></div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
